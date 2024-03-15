d974ec84f9adb1cd4c79bdd8d4d285fe38dc508c
Martorad W <martin.100ikov@gmail.com>
2024-03-15 10:28:26 +0100

    Added some more FOV configs, which actually don't control FOV at all, but rather resolution

--- Core/Inc/rc_config.h
+++ Core/Inc/rc_config.h
@@ -13,37 +13,55 @@
 // ------------------------ DO NOT TOUCH ------------------------
 // INTERNAL CONFIGURATION DEFINES
 #define M_3PI_2  4.71238898038468985769
-#define FOV_INCR 0.0174533
 
 // FOV CONFIGURATION TABLES
-#ifdef FOV_1
+#ifdef  FOV_0
+#define FOV      30
+#define FOV_HALF 15
+#define FOV_RECT 16
+#define FOV_INCR 0.0349066
+#endif
+
+#ifdef  FOV_1
 #define FOV      60
 #define FOV_HALF 30
 #define FOV_RECT 8
+#define FOV_INCR 0.0174533
 #endif
 
-#ifdef FOV_2
+#ifdef  FOV_2
 #define FOV      80
 #define FOV_HALF 40
 #define FOV_RECT 6
+#define FOV_INCR 0.013089975
 #endif
 
-#ifdef FOV_3
+#ifdef  FOV_3
 #define FOV      96
 #define FOV_HALF 48
 #define FOV_RECT 5
+#define FOV_INCR 0.0109083125
 #endif
 
-#ifdef FOV_4
+#ifdef  FOV_4
 #define FOV      120
 #define FOV_HALF 60
 #define FOV_RECT 4
+#define FOV_INCR 0.00872665
 #endif
 
-#ifdef FOV_5
+#ifdef  FOV_5
 #define FOV      240
 #define FOV_HALF 120
 #define FOV_RECT 2
+#define FOV_INCR 0.004363325
+#endif
+
+#ifdef  FOV_6
+#define FOV      480
+#define FOV_HALF 240
+#define FOV_RECT 1
+#define FOV_INCR 0.0021816625
 #endif
 
 #endif
