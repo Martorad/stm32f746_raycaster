88bccb53395b94ac10b84b38a923cf18fb4db85a
Martorad W <martin.100ikov@gmail.com>
2024-03-28 14:53:22 +0100

    Changed define naming scheme to better reflect its purpose

--- src_v1/Core/Inc/rc_config.h
+++ src_v1/Core/Inc/rc_config.h
@@ -2,7 +2,7 @@
 #define __RC__CONFIG__H__
 
 // PERFORMANCE SETTINGS
-#define FOV_5
+#define RES_5
 #define REMOVE_FISHEYE
 
 #define DOF 24
@@ -22,57 +22,58 @@
 #define M_3PI_2  4.71238898038468985769
 #define R_HIT    0xAAAA
 
-// FOV CONFIGURATION TABLES
-#ifdef  FOV_0
+// RESOLUTION CONFIGURATION TABLES
+// FOV_RECT describes the width of each rectangle drawn on screen. Smaller width rectangles have higher fidelity, at the cost of speed. Odd width rectangles mean that symmetrical frames cannot be drawn.
+#ifdef  RES_0
 #define FOV      30
 #define FOV_HALF 15
 #define FOV_RECT 16
 #define FOV_INCR 0.0349066
 #endif
 
-#ifdef  FOV_1
+#ifdef  RES_1
 #define FOV      60
 #define FOV_HALF 30
 #define FOV_RECT 8
 #define FOV_INCR 0.0174533
 #endif
 
-#ifdef  FOV_2
+#ifdef  RES_2
 #define FOV      80
 #define FOV_HALF 40
 #define FOV_RECT 6
 #define FOV_INCR 0.013089975
 #endif
 
-#ifdef  FOV_3
+#ifdef  RES_3
 #define FOV      96
 #define FOV_HALF 48
 #define FOV_RECT 5
 #define FOV_INCR 0.0109083125
 #endif
 
-#ifdef  FOV_4
+#ifdef  RES_4
 #define FOV      120
 #define FOV_HALF 60
 #define FOV_RECT 4
 #define FOV_INCR 0.00872665
 #endif
 
-#ifdef  FOV_5
+#ifdef  RES_5
 #define FOV      160
 #define FOV_HALF 80
 #define FOV_RECT 3
 #define FOV_INCR 0.005817767
 #endif
 
-#ifdef  FOV_6
+#ifdef  RES_6
 #define FOV      240
 #define FOV_HALF 120
 #define FOV_RECT 2
 #define FOV_INCR 0.004363325
 #endif
 
-#ifdef  FOV_7
+#ifdef  RES_7
 #define FOV      480
 #define FOV_HALF 240
 #define FOV_RECT 1
