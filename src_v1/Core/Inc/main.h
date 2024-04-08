ef1475fcef03556452d3f1427b0a3c60b56ec979
Martorad W <martin.100ikov@gmail.com>
2024-04-08 14:32:37 +0200

    Reordered some function definitions

--- src_v1/Core/Inc/main.h
+++ src_v1/Core/Inc/main.h
@@ -54,12 +54,12 @@ void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
 uint32_t cast();
-float    rayLength(float ax, float ay, float bx, float by);
-float    fsqrt(float x);
-void     pageFlip();
 uint32_t CLUT(uint8_t index, uint8_t hitSide);
 uint32_t dimColor(uint32_t inputColor, float dimmingFactor);
+float    rayLength(float ax, float ay, float bx, float by);
+float    fsqrt(float x);
 void     showFPS(uint32_t frameTime);
+void     pageFlip();
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
