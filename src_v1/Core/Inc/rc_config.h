6e6ea751f06ab972900419585291a91fc0c9b2c1
Martorad W <martin.100ikov@gmail.com>
2024-04-12 15:16:39 +0200

    Increased player hitbox size. Reverted to O2

--- src_v1/Core/Inc/rc_config.h
+++ src_v1/Core/Inc/rc_config.h
@@ -13,7 +13,7 @@
 // MOVEMENT SETTINGS
 #define INCR_ROTATION    0.0002  // Higher is faster
 #define INCR_TRANSLATION 0.0012  // Higher is faster
-#define P_HITBOX_SIZE    0.3     // In units of map squares
+#define P_HITBOX_SIZE    0.6     // In units of map squares
 
 // DEBUG SETTINGS
 //#define DEBUG_FULLBRIGHT
