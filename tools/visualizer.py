11e0ef8049ac996082a6bf2b2cc0b3a6ce9e4258
Martorad W <martin.100ikov@gmail.com>
2024-04-29 14:12:37 +0200

    Modified to accept RGB565 format

--- tools/visualizer.py
+++ tools/visualizer.py
@@ -31,11 +31,26 @@ def create_image_from_colors(filename, imagesize):
         for x in range(width):
             color_index = y * width + x
             if color_index < num_colors:
-                color = hex_values[color_index]
+                color = rgb565_to_rgb888(hex_values[color_index])
                 image.putpixel((x, y), ((color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF))
 
     # Save or display the image
     image.show()
+
+def rgb565_to_rgb888(rgb565):
+    # Extract individual color components from RGB565
+    red = (rgb565 >> 11) & 0x1F
+    green = (rgb565 >> 5) & 0x3F
+    blue = rgb565 & 0x1F
+
+    # Scale color components to fit into 8-bit range (0-255)
+    red = (red * 255) // 31
+    green = (green * 255) // 63
+    blue = (blue * 255) // 31
+
+    # Combine color components into RGB888 format
+    rgb888 = (red << 16) | (green << 8) | blue
+    return rgb888
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
